967d3b76c94344f228b86cb6fad0b1c8cd2f41d3
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-14 01:23:21 +0700

    set checkbox, imgplay and seekbar

--- app/src/main/java/com/example/duongtainhan555/seekbarracing/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/seekbarracing/MainActivity.java
@@ -1,13 +1,119 @@
 package com.example.duongtainhan555.seekbarracing;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox ckOne, ckTwo, ckThree;
+    private SeekBar sbOne, sbTwo, sbThree;
+    private ImageView imgPlay;
+    private Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AnhXa();
+        eventClick();
     }
+
+    private void AnhXa(){
+        //Anh xa
+        ckOne = findViewById(R.id.checkboxOne);
+        ckTwo = findViewById(R.id.checkboxTwo);
+        ckThree = findViewById(R.id.checkboxThree);
+        sbOne = findViewById(R.id.seekbarOne);
+        sbTwo = findViewById(R.id.seekbarTwo);
+        sbThree = findViewById(R.id.seekbarThree);
+        imgPlay = findViewById(R.id.imgPlay);
+    }
+    private void eventClick() {
+        imgPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CountDownTimer countDownTimer = new CountDownTimer(60000, 500) {
+                    @Override
+                    public void onTick(long l) {
+                        random = new Random();
+                        int randomOne = random.nextInt(10) + 1;
+                        int randomTwo = random.nextInt(10) + 1;
+                        int randomThree = random.nextInt(10) + 1;
+
+                        sbOne.setProgress(sbOne.getProgress() + randomOne);
+                        sbTwo.setProgress(sbTwo.getProgress() + randomTwo);
+                        sbThree.setProgress(sbThree.getProgress() + randomThree);
+
+                        if (sbOne.getProgress() >= 100) {
+                            Toast.makeText(MainActivity.this, "Tiger Win", Toast.LENGTH_SHORT);
+                            this.cancel();
+                            imgPlay.setEnabled(true);
+                        } else if (sbTwo.getProgress() >= 100) {
+                            Toast.makeText(MainActivity.this, "Buffalo Win", Toast.LENGTH_SHORT);
+                            this.cancel();
+                            imgPlay.setEnabled(true);
+                        } else if (sbThree.getProgress() >= 100) {
+                            Toast.makeText(MainActivity.this, "Cat Win", Toast.LENGTH_SHORT);
+                            this.cancel();
+                            imgPlay.setEnabled(true);
+
+                        }
+                    }
+
+                    @Override
+                    public void onFinish() {
+
+                    }
+                };
+                countDownTimer.start();
+                imgPlay.setEnabled(false);
+            }
+        });
+        ckOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(ckOne.isChecked())
+                {
+                    ckOne.setChecked(true);
+                    ckTwo.setChecked(false);
+                    ckThree.setChecked(false);
+                }
+            }
+        });
+        ckTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(ckTwo.isChecked())
+                {
+                    ckTwo.setChecked(true);
+                    ckOne.setChecked(false);
+                    ckThree.setChecked(false);
+                }
+            }
+        });
+        ckThree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(ckThree.isChecked())
+                {
+                    ckThree.setChecked(true);
+                    ckTwo.setChecked(false);
+                    ckOne.setChecked(false);
+                }
+            }
+        });
+    }
+
+
 }
